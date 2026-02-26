fc2d4172acac0573607bd1907dfbc78c043ad196
Teerth Patel <teerthp2004@gmail.com>
2026-02-26 15:30:30 -0600

    Remove auth check on image upload: already implemented in proxy.ts

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,13 +1,9 @@
 import {NextRequest, NextResponse} from "next/server";
 import path from "node:path";
 import {writeFile} from "node:fs/promises";
-import {verifySession} from "@/lib/session"
 import {createId} from "@paralleldrive/cuid2"
 
 export async function POST(req: NextRequest) {
-  if (!(await verifySession())) {
-    return NextResponse.json({error: {message: 'Unauthorized'}}, {status: 401});
-  }
   const formData = await req.formData();
   const file = formData.get("file") as File;
   const buffer = new Uint8Array(await file.arrayBuffer());
