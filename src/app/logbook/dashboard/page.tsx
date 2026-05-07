6e0dbd7fef2f24a16e905818ca78a3961cf9bf81
amrodais <samrod2301@gmail.com>
2026-05-07 15:43:11 -0500

    re-adding lost dashboard UX changes/darkening of legend

--- src/app/logbook/dashboard/page.tsx
+++ src/app/logbook/dashboard/page.tsx
@@ -39,7 +39,15 @@ export default function Dashboard(
                         currentUser={currentUser} isAdminMode={isAdminMode}
                         selectedProduct={selectedProduct} selectedUser={selectedUser} selectedMonth={selectedMonth}
                     />
-                    
+
+                    {/* giving context to number in middle of the pi chart. "a tiempo" is on time, which is On Time + early tasks
+                        NOTE: must change the percentage both in the pi chart and the statement to fit actual data!
+                        for now just dummy data. */}
+                    <p className={styles.summaryText}>
+                        Completaste el 100% de tus tareas este mes.
+                    </p>
+
+
                     <h6 className={styles.insideHeader}>Tareas</h6>
                     <TasksDashboard 
                         currentUser={currentUser} isAdminMode={isAdminMode}
